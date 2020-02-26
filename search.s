155165102764fd250a64de9076d9c722d3d508ac
Justus Schmidt <32693986+jgs377@users.noreply.github.com>
2020-02-25 20:32:31 -0600

    formatting

--- search.s
+++ search.s
@@ -17,13 +17,13 @@ BinarySearch:
 	move.l a6,-(a7) *save caller's frame pointer
 	move.l a7,a6	*setup my own frame pointer
 
-*set up mid point mid = (low + high) / 2
-	move.l 12(a6),D1 	*move low to D1
-	add.l 8(a6),D1		*add high to low in D1
-	divs.w #2,D1		*divide by 2
+	*set up mid point mid = (low + high) / 2
+	move.l 12(a6),D1 		*move low to D1
+	add.l 8(a6),D1			*add high to low in D1
+	divs.w #2,D1			*divide by 2
 	ext.l D1			*remove remainder
 
-*base (failure) case
+	*base (failure) case
 if1:
 	move.l 12(a6),D2 		*low to D2
 	move.l 8(a6),D3 		*high to D3
@@ -39,11 +39,11 @@ endif1:
 
 	movea.l 20(a6),a0		*move address of array to a0
 	move.l D1,D6			*make a copy of mid in d6
-	muls #4,D6				*multiply by 4 because of long type
-	move.l 0(a0,D6.l),D4	*use d6.l as offset for middle element
+	muls #4,D6			*multiply by 4 because of long type
+	move.l 0(a0,D6.l),D4		*use d6.l as offset for middle element
 	move.l 16(a6),D5		*move key to d5
 
-	cmp.l D4,D5				*if(key == A[mid])
+	cmp.l D4,D5			*if(key == A[mid])
 	BNE ne
 	
 	move.l a6,a7			*if this code is reached, then the key
@@ -53,12 +53,12 @@ endif1:
 
 ne:
 
-	BGE ge					*(if key < A[mid]
+	BGE ge				*(if key < A[mid]
 
 	move.l 20(a6),-(a7)		*move #A onto stack
 	move.l 16(a6),-(a7)		*move key onto stack
 	move.l 12(a6),-(a7)		*move low onto stack
-	sub.l #1,D1				*subtract 1 from midpoint 
+	sub.l #1,D1			*subtract 1 from midpoint 
 	move.l D1,-(a7)			*move midpoint-1 onto stack
 	jsr BinarySearch		*BinarySearch(#A,key,low,mid-1)
 	adda.l #16,a7			*remove 4 parameters
@@ -70,7 +70,7 @@ ge:
 
 	move.l 20(a6),-(a7)		*move #A onto stack
 	move.l 16(a6),-(a7)		*move key onto stack
-	add.l #1,D1				*add 1 to midpoint
+	add.l #1,D1			*add 1 to midpoint
 	move.l D1,-(a7)			*move midpoint+1 onto stack
 	move.l D3,-(a7)			*move high onto stack
 	jsr BinarySearch		*BinarySearch(#A,key,mid+1,high)
